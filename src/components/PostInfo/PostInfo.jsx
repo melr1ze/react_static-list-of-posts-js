8f6c4c18cd1425790dc0370ce2e2e0236deecf91
Timur K <timurkakandzo@gmail.com>
2026-06-29 18:45:38 +0300

    final2

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -3,22 +3,21 @@ import { CommentList } from '../CommentList';
 import { UserInfo } from '../UserInfo';
 
 export const PostInfo = ({ post, user, comments }) => {
+  const postUser = user ?? post.user;
+  const postComments = comments ?? post.comments;
+
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{post.title}</h3>
         <p>
           {' Posted by  '}
-
-          {user && <UserInfo user={user} />}
+          {postUser && <UserInfo user={postUser} />}
         </p>
       </div>
-
       <p className="PostInfo__body">{post.body}</p>
-
       <hr />
-
-      <CommentList comments={comments} />
+      <CommentList comments={postComments} />
     </div>
   );
 };
